2d10d0a04ac139bacb861e6e5cd74ab4c9f93fe5
Elliot Rodriguez <elliotrodriguez72@gmail.com>
2025-01-01 16:13:58 -0500

    first commit, test mac splash screen icon

--- ios/teapot/AppDelegate.mm
+++ ios/teapot/AppDelegate.mm
@@ -1,6 +1,10 @@
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+// Import RNSplashScreen
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
@@ -10,8 +14,21 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                     moduleName:@"MySplashScreen"
+                                              initialProperties:nil];
+    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *rootViewController = [UIViewController new];
+    rootViewController.view = rootView;
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+    // Set the splash screen to show by default.
+    [RNSplashScreen show];
+    return YES;
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  //return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
